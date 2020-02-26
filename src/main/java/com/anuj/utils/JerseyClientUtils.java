5f08ee22ca6bb13493f1ca16fafffc30a603e73e
Anuj Dayal Bharti <anuj007a@gmail.com>
2020-02-26 19:08:28 +0530

    Logger

--- src/main/java/com/anuj/utils/JerseyClientUtils.java
+++ src/main/java/com/anuj/utils/JerseyClientUtils.java
@@ -1,6 +1,7 @@
 package com.anuj.utils;
 
-import org.apache.logging.log4j.core.Logger;
+import com.anuj.base.MasterLogger;
+import org.apache.logging.log4j.Logger;
 import org.glassfish.jersey.client.ClientResponse;
 import org.json.JSONObject;
 
@@ -9,7 +10,8 @@ import javax.ws.rs.client.ClientBuilder;
 import java.util.HashMap;
 
 public class JerseyClientUtils {
-        static Logger log = null;
+    public static Logger log = MasterLogger.getInstance();
+
     static ClientResponse post(String url, HashMap<Object, Object> header, JSONObject requestBody){
         ClientResponse clientResponse = null;
         Client client = ClientBuilder.newClient();
